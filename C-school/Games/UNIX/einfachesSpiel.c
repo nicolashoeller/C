897d2b322ebbecf3a1363b40331743ebea7b655b
nicolashoeller <hoellr3.0@gmail.com>
2023-11-15 09:19:26 +0100

    added score and lifes

--- C-school/Games/UNIX/einfachesSpiel.c
+++ C-school/Games/UNIX/einfachesSpiel.c
@@ -13,11 +13,14 @@ int main()
 
     int gr1 = 0;
     int gr2 = 0;
+    int rdm1 = 0;
+    int rdm2 = 0;
     int x = 0;
     int y = 0;
     int forCountZ = 0;
     int dead = 0;
     int score = 0;
+    int leben = 3;
     char spieler = 'X';
     char input = 0;
 
@@ -32,7 +35,7 @@ int main()
     x = gr1 / 2;
     y = gr2 / 2;
 
-    //Feld füllen
+    // Feld füllen
     for (int i = 0; i < gr1; i++)
     {
         for (int j = 0; j < gr2; j++)
@@ -41,29 +44,31 @@ int main()
         }
     }
 
-    //Feld mit Z und O füllen
-    for (int i = 0; i < gr1; i++)
+    // Feld mit Z und O füllen
+    for (int i = 0; i < 3; i++)
     {
-        spielfeld[rand()%gr1][rand()%gr2] = 'Z';
+        spielfeld[rand() % gr1][rand() % gr2] = 'Z';
     }
-    for (int i = 0; i < gr1*1.5; i++)
+    for (int i = 0; i < 4; i++)
     {
-        spielfeld[rand()%gr1][rand()%gr2] = 'O';
+        spielfeld[rand() % gr1][rand() % gr2] = 'O';
     }
     spielfeld[x][y] = spieler;
-
-    //Feld ausgeben
-    for (int i = 0; i < gr1; i++)
+    while (leben > 0)
     {
-        for (int j = 0; j < gr2; j++)
+        rdm1 = rand() % gr1;
+        rdm2 = rand() % gr1;
+        if (spielfeld[rdm1][rdm2] == '-')
         {
-            printf("| %c ", spielfeld[i][j]);
+            spielfeld[rand() % gr1][rand() % gr2] = 'Z';
         }
-        printf("|\n");
-    }
+        rdm1 = rand() % gr1;
+        rdm2 = rand() % gr1;
+        if (spielfeld[rdm1][rdm2] == '-')
+        {
+            spielfeld[rand() % gr1][rand() % gr2] = 'O';
+        }
 
-    while (!dead)
-    {
         fflush(stdin);
         input = getchar();
         system("clear");
@@ -75,6 +80,8 @@ int main()
             }
             printf("|\n");
         }
+        printf("\nScore: %d", score);
+        printf("\nLeben: %d\n", leben);
 
         if (input == 'w')
         {
@@ -84,15 +91,16 @@ int main()
                 x = gr1;
             }
             x--;
-            if(spielfeld[x][y] == 'Z')
+            if (spielfeld[x][y] == 'Z')
             {
-                printf("\n-----------------------------\nDu hasch verloren klein Pisser du!");
-                dead = 1;
+                leben--;
             }
-            else
-            {
-                spielfeld[x][y] = 'X';
-            }           
+            if (spielfeld[x][y] == 'O')
+        {
+            score+=10;
+        }
+
+            spielfeld[x][y] = 'X';
         }
         else if (input == 'a')
         {
@@ -102,17 +110,18 @@ int main()
                 x++;
             }
             y--;
-            if(spielfeld[x][y] == 'Z')
+            if (spielfeld[x][y] == 'Z')
             {
-                printf("\n-----------------------------\nDu hasch verloren klein Pisser du!");
-                dead = 1;
+                leben--;
             }
-            else
-            {
-                spielfeld[x][y] = 'X';
-            }  
+            if (spielfeld[x][y] == 'O')
+        {
+            score+=10;
         }
-        else if (input == 's') //Funkt net
+
+            spielfeld[x][y] = 'X';
+        }
+        else if (input == 's') // Funkt net
         {
             spielfeld[x][y] = '-';
             if (y == gr1)
@@ -120,17 +129,18 @@ int main()
                 x = 0;
             }
             x++;
-            if(spielfeld[x][y] == 'Z')
+            if (spielfeld[x][y] == 'Z')
             {
-                printf("\n-----------------------------\nDu hasch verloren klein Pisser du!");
-                dead = 1;
+                leben--;
             }
-            else
-            {
-                spielfeld[x][y] = 'X';
-            }  
+            if (spielfeld[x][y] == 'O')
+        {
+            score+=10;
         }
-        else if (input == 'd') //Funkt net
+
+            spielfeld[x][y] = 'X';
+        }
+        else if (input == 'd') // Funkt net
         {
             spielfeld[x][y] = '-';
             if (x == gr1)
@@ -138,26 +148,29 @@ int main()
                 x--;
             }
             y++;
-            if(spielfeld[x][y] == 'Z')
+            if (spielfeld[x][y] == 'Z')
             {
-                printf("\n-----------------------------\nDu hasch verloren klein Pisser du!");
-                dead = 1;
+
+                leben--;
             }
-            else
-            {
-                spielfeld[x][y] = 'X';
-            }  
+            if (spielfeld[x][y] == 'O')
+        {
+            score+=10;
         }
+
+            spielfeld[x][y] = 'X';
+        }
+
         else if (input == 'x')
         {
             printf("\n-----------------------------\nDas Spiel wurde beendet!!");
-            dead = 1;
+            dead = 3;
         }
-        
+
         else
         {
             printf("Steuerung mit w, a, s, d");
         }
-        
     }
+    printf("\n-----------------------------\nDu hasch verloren klein Pisser!!");
 }
