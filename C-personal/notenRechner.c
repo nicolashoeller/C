517574129cba505220ca011d52373b36da13f163
nicolashoeller <hoellr3.0@gmail.com>
2023-11-26 19:31:06 +0100

    calculator for grades

---
+++ C-personal/notenRechner.c
@@ -0,0 +1,20 @@
+/*
+File: noten.c
+Date: 26.11.2023
+Autor: Nicolas Höller
+*/
+#include <stdio.h>
+
+int main()
+{
+    int note[10] = 0;
+    int i=0;
+    
+    while (note[i])
+    {
+        /* code */
+    }
+    
+    printf("Gib deine Note ein: ");
+    scanf("%d", &note);
+}
