6716db24b3fdc53b5f83f5104a375ef49e1aec5d
nicolashoeller <hoellr3.0@gmail.com>
2023-11-27 12:33:17 +0100

    amount of number in betrag fx

---
+++ C-school/Funktionen/betrag.c
@@ -0,0 +1,32 @@
+/*
+File: betrag.c
+Date: 27.11.2023
+Autor: Nicolas Höller
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int betrag(int);
+
+int main()
+{
+    int eingabe = 0;
+    printf("Gib deine Zahl ein: ");
+    scanf("%d", &eingabe);
+    eingabe = betrag(eingabe);
+    printf("%d", eingabe);
+}
+
+int betrag(int eingabe){
+    if (eingabe >= 0)
+    {
+        return eingabe;
+    }
+    else
+    {
+        eingabe*=(-1);
+        return eingabe;
+    }
+    
+    
+}
