0197a93690a3d1a8be0168119ce9edf229d3ee0a
nicolashoeller <hoellr3.0@gmail.com>
2023-12-04 18:27:56 +0100

    changed thing for windows

---
+++ C-school/Games/Windows/Labyrinth/labyrinth.c
@@ -0,0 +1,173 @@
+/*
+File: labyrinth.c
+Date: 30.11.2023
+Autor: Nicolas Höller
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+// Function index
+
+void welcome();
+void spielfeldPrint(int xKoordinate, int yKoordinate, char spielfeld[xKoordinate][yKoordinate], int versuche);
+int playerWin(char player, int xKoordinate, int yKoordinate, char spielfeld[xKoordinate][yKoordinate]);
+
+int main()
+{
+    srand(time(NULL));
+    int xKoordinate = 11;
+    int yKoordinate = 20;
+    int height = xKoordinate;
+    int whidth = yKoordinate;
+    int zufZahl = 0;
+    char player = 'X';
+    int versuche = 0;
+    int win = 0;
+
+    char spielfeld[xKoordinate][yKoordinate];
+
+    welcome();
+
+    // Initialisiert array
+
+    for (int i = 0; i < xKoordinate; i++)
+    {
+        for (int j = 0; j < yKoordinate; j++)
+        {
+            spielfeld[i][j] = '*';
+        }
+    }
+
+    xKoordinate--;
+    yKoordinate /= 2;
+
+    spielfeld[xKoordinate][yKoordinate] = 'X';
+
+    while (!win)
+    {
+        spielfeldPrint(height, whidth, spielfeld, versuche);
+        sleep(1);
+        spielfeld[xKoordinate][yKoordinate] = '*';
+        zufZahl = rand() % 8;
+        switch (zufZahl)
+        {
+        case 0:
+            xKoordinate--;
+            break;
+        case 1:
+            xKoordinate++;
+            break;
+        case 2:
+            yKoordinate--;
+            break;
+        case 3:
+            yKoordinate++;
+            break;
+        case 4:
+            xKoordinate++;
+            yKoordinate++;
+            break;
+        case 5:
+            xKoordinate--;
+            yKoordinate++;
+            break;
+        case 6:
+            xKoordinate++;
+            yKoordinate--;
+            break;
+        case 7:
+            xKoordinate--;
+            yKoordinate--;
+            break;
+        default:
+            break;
+        }
+
+        if (xKoordinate >= 0 && xKoordinate < 11 && yKoordinate >= 0 && yKoordinate < 20)
+        {
+            spielfeld[xKoordinate][yKoordinate] = player;
+            if (playerWin(player, height, whidth, spielfeld))
+            {
+                spielfeld[xKoordinate - 1][yKoordinate] = player;
+                spielfeldPrint(height, whidth, spielfeld, versuche);
+                printf("\nLoter er hots gschofft!!\n");
+                printf("\nEs Verhältnis, dass der bsoffene durchkimmt liegt pa ca. 1/%d", versuche);
+                win = 1;
+            }
+        }
+        else if (xKoordinate < 0 || xKoordinate >= 11 || yKoordinate < 0 || yKoordinate >= 20)
+        {
+            printf("\nJetz hobn sie ihn gfassen, soll er lei no amol probieren!\n");
+            xKoordinate = height - 1;
+            yKoordinate = whidth / 2;
+            spielfeld[xKoordinate][yKoordinate] = 'X';
+            versuche++;
+            sleep(3);
+        }
+    }
+
+    return 0;
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+int playerWin(char player, int xKoordinate, int yKoordinate, char spielfeld[xKoordinate][yKoordinate])
+{
+    for (int i = 0; i < yKoordinate; i++)
+    {
+        if (spielfeld[0][i] == player)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void spielfeldPrint(int xKoordinate, int yKoordinate, char spielfeld[xKoordinate][yKoordinate], int versuche)
+{
+    system("cls");
+    printf("\nPass au, der hot GLUUHWEIN GSOFFEN!!\n---------------------------\n");
+
+    for (int i = 0; i < xKoordinate; i++)
+    {
+        printf("||");
+        for (int j = 0; j < yKoordinate; j++)
+        {
+            printf("%c", spielfeld[i][j]);
+        }
+        printf("||\n");
+    }
+
+    printf("\nVeruche: %d", versuche);
+}
+
+void welcome()
+{
+    system("cls");
+    printf("\n\n");
+    printf("██      ███████ ████████ ███████ \n");
+    printf("██      ██         ██    ██      \n");
+    printf("██      █████      ██    ███████ \n");
+    printf("██      ██         ██         ██ \n");
+    printf("███████ ███████    ██    ███████ \n");
+    sleep(1);
+    system("cls");
+    printf("\n\n");
+    printf("███████  █████  ██    ██ ███████ \n");
+    printf("██      ██   ██ ██    ██ ██      \n");
+    printf("███████ ███████ ██    ██ █████   \n");
+    printf("     ██ ██   ██ ██    ██ ██      \n");
+    printf("███████ ██   ██  ██████  ██      \n");
+    sleep(1);
+    system("cls");
+    printf("                 ██    ██\n");
+    printf("\n");
+    printf(" ██████  ██      ██    ██ ██   ██ ██     ██ ███████ ██ ███    ██ \n");
+    printf("██       ██      ██    ██ ██   ██ ██     ██ ██      ██ ████   ██ \n");
+    printf("██   ███ ██      ██    ██ ███████ ██  █  ██ █████   ██ ██ ██  ██ \n");
+    printf("██    ██ ██      ██    ██ ██   ██ ██ ███ ██ ██      ██ ██  ██ ██\n");
+    printf(" ██████  ███████  ██████  ██   ██  ███ ███  ███████ ██ ██   ████ \n");
+    sleep(1);
+}
