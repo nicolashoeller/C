ac8b35fb42dff15442706b9346c65edeba61899b
nicolashoeller <hoellr3.0@gmail.com>
2023-12-13 21:08:40 +0100

    added functions

--- C-school/Übergabeparameter/parameterToArray.c
+++ C-school/Übergabeparameter/parameterToArray.c
@@ -6,9 +6,20 @@ Autor: Nicolas Höller
 #include <stdio.h>
 #include <stdlib.h>
 
+void store(int argc, char* argv[argc], int array[argc]);
+void printArray(int argc, int array[argc]);
+
 int main(int argc, char* argv[])
 {
     int array[argc];
+
+    store(argc, argv, array);
+    printArray(argc, array);
+    
+    return 0;
+}
+
+void store(int argc, char* argv[argc], int array[argc]){
     int values = 0;
 
     for (int i = 0; i < argc; i++)
@@ -16,11 +27,11 @@ int main(int argc, char* argv[])
         values = atoi(argv[i]);
         array[i] = values;
     }
+}
+
+void printArray(int argc, int array[argc]){
     for (int i = 0; i < argc; i++)
     {
         printf("%d\n", array[i]);
     }
-
-    
-    return 0;
 }
