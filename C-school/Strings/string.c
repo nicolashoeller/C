84c62c5438091ba0b1025ed437e53b8e36845b55
nicolashoeller <hoellr3.0@gmail.com>
2023-12-18 13:08:20 +0100

    calculate string length

---
+++ C-school/Strings/string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calculate(char*);
+
+int main(){
+    char string[] = "Hello World!";
+    
+    int result = calculate(string);
+
+    printf("%d", result);
+
+    return 0;
+}
+
+int calculate(char* string){
+    int length = 0;
+    
+    for (int i = 0; string[i] != '\0'; i++) {
+        length++;
+    }
+
+    return length;
+}
