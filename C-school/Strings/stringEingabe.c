d1aa9ab185220cfa3b6acfa07d030b7343f52049
nicolashoeller <hoellr3.0@gmail.com>
2023-12-20 15:30:07 +0100

    convert string

---
+++ C-school/Strings/stringEingabe.c
@@ -0,0 +1,44 @@
+/*
+File: stringEingabe.c
+Date: 20.12.2023
+Autor: Nicolas Höller
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    char string[50];
+    int option;
+
+    printf("Bitte geben Sie einen String ein: ");
+    scanf("%s", string);
+
+    printf("Soll der String in Großbuchstaben (1) oder in Kleinbuchastaben (2) ausgegeben werden?\n", string);
+    scanf("%d", &option);
+
+    switch (option)
+    {
+        case 1:
+            for (int i = 0; string[i] != '\0'; i++)
+            {
+                if (string[i] >= 'a' && string[i] <= 'z')
+                {
+                    string[i] = string[i] - 32;
+                }
+            }
+            printf("%s", string);
+            break;
+        case 2: 
+            for (int i = 0; string[i] != '\0'; i++)
+            {
+                if (string[i] >= 'A' && string[i] <= 'Z')
+                {
+                    string[i] = string[i] + 32;
+                }
+            }
+            printf("%s", string);
+            break;
+    }
+}
