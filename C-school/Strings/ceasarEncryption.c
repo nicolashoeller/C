5c5106a9a972e4569997328066294f0fdba83e20
nicolashoeller <hoellr3.0@gmail.com>
2023-12-21 09:02:56 +0100

    removed useless array

--- C-school/Strings/ceasarEncryption.c
+++ C-school/Strings/ceasarEncryption.c
@@ -12,10 +12,6 @@ Autor: Nicolas Höller
 
 int main(int argc, char* argv[])
 {
-    char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                      'u', 'v', 'w', 'x', 'y', 'z'};
-
     char stringEingabe[MAX];
     int verschiebung = 0;
 
