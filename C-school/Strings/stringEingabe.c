e5a571ad7319de17be374e40ad81234a73ddc2ca
nicolashoeller <hoellr3.0@gmail.com>
2023-12-21 08:28:34 +0100

    no output in processingString function

--- C-school/Strings/stringEingabe.c
+++ C-school/Strings/stringEingabe.c
@@ -6,8 +6,52 @@ Autor: Nicolas Höller
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void processString(char string[], int option)
+#define MAX 100
+
+void processString(char*, int);
+void capsAfterSpace(char*);
+
+int main(int argc, char* argv[])
+{
+    char string[MAX];
+    int option;
+
+    printf("Bitte geben Sie einen String ein: ");
+    fgets(string, MAX, stdin);                          // fgets liest auch Leerzeichen ein, stdin ist der Standardinput (Tastatur)
+
+    printf("Soll der String in Großbuchstaben (1) oder in Kleinbuchstaben (2) ausgegeben werden?\n", string);
+    scanf("%d", &option);
+
+    processString(string, option);
+    capsAfterSpace(string);
+    printf("%s", string);
+
+    return 0;
+}
+
+void capsAfterSpace(char* string){
+    if (string[0] >= 'a' && string[0] <= 'z')
+    {
+        string[0] = string[0] - 32;
+    }
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        if (string[i] == ' ')
+        {
+            if (string[i+1] >= 'a' && string[i+1] <= 'z')
+            {
+                string[i+1] = string[i+1] - 32;
+            }
+            
+        }
+        
+    }
+    
+}
+
+void processString(char* string, int option)
 {
     switch (option)
     {
@@ -19,7 +63,6 @@ void processString(char string[], int option)
                     string[i] = string[i] - 32;
                 }
             }
-            printf("%s", string);
             break;
         case 2: 
             for (int i = 0; string[i] != '\0'; i++)
@@ -29,23 +72,6 @@ void processString(char string[], int option)
                     string[i] = string[i] + 32;
                 }
             }
-            printf("%s", string);
             break;
     }
-}
-
-int main(int argc, char* argv[])
-{
-    char string[50];
-    int option;
-
-    printf("Bitte geben Sie einen String ein: ");
-    scanf("%s", string);
-
-    printf("Soll der String in Großbuchstaben (1) oder in Kleinbuchstaben (2) ausgegeben werden?\n", string);
-    scanf("%d", &option);
-
-    processString(string, option);
-
-    return 0;
 }
