411d57388dd9cbc76fa159bac5430027700e0d95
nicolashoeller <hoellr3.0@gmail.com>
2023-12-21 09:00:46 +0100

    finished encryption

--- C-school/Strings/ceasarEncryption.c
+++ C-school/Strings/ceasarEncryption.c
@@ -8,14 +8,30 @@ Autor: Nicolas Höller
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 100
+
 int main(int argc, char* argv[])
 {
     char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                       'u', 'v', 'w', 'x', 'y', 'z'};
 
-    char stringEingabe[100];
+    char stringEingabe[MAX];
+    int verschiebung = 0;
 
     printf("Bitte geben Sie einen String ein: ");
-    fgets(stringEingabe, 100, stdin);
+    fgets(stringEingabe, MAX, stdin);
+    printf("Um wie viele Stellen soll der String verschoben werden? ");
+    scanf("%d", &verschiebung);
+
+    for (int i = 0; stringEingabe[i] != '\0'; i++)
+    {
+        if (stringEingabe[i] >= 'a' && stringEingabe[i] <= 'z') {
+            stringEingabe[i] = ((stringEingabe[i] - 'a' + verschiebung) % 26) + 'a';
+        }
+    }
+
+
+    printf("Der verschlüsselte String lautet: %s", stringEingabe);
+    return 0;
 }
