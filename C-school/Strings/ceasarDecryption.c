7a90478410378e41142df53df487572c71a994e7
nicolashoeller <hoellr3.0@gmail.com>
2023-12-22 14:11:19 +0100

    changed a few things

--- C-school/Strings/ceasarDecryption.c
+++ C-school/Strings/ceasarDecryption.c
@@ -10,22 +10,41 @@ Autor: Nicolas Höller
 
 #define MAX 100
 
-int stringInput(char*);
-void ceasarDecryption(char*, int);
+int stringInput(char *);
+void ceasarDecryption(char *, int);
+void ceasarEncryption(char *);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     char stringEingabe[MAX];
     int verschiebung = 0;
+    int auswahl = 0;
 
-    verschiebung = stringInput(stringEingabe);
-    ceasarDecryption(stringEingabe, verschiebung);
-    printf("Der verschlüsselte String lautet: %s", stringEingabe);
+    printf("Möchten Sie einen String verschlüsseln (1) oder entschlüsseln (2)? ");
+    scanf("%d", &auswahl);
+
+    if (auswahl == 1)
+    {
+        verschiebung = stringInput(stringEingabe);
+        ceasarDecryption(stringEingabe, verschiebung);
+        printf("Der verschlüsselte String lautet: %s", stringEingabe);
+    }
+    else if (auswahl == 2)
+    {
+        fflush(stdin);
+        fgets(stringEingabe, MAX, stdin);
+        ceasarEncryption(stringEingabe);
+        printf("Der entschlüsselte String lautet: %s", stringEingabe);
+    }
+    else
+    {
+        printf("Ungültige Eingabe!");
+    }
 
     return 0;
 }
 
-int stringInput(char* string)
+int stringInput(char *string)
 {
     int verschiebung = 0;
     printf("Bitte geben Sie einen String ein: ");
@@ -36,12 +55,28 @@ int stringInput(char* string)
     return verschiebung;
 }
 
-void ceasarDecryption(char* string, int verschiebung)
+void ceasarEncryption(char *string)
 {
     for (int i = 0; string[i] != '\0'; i++)
     {
-        if (string[i] >= 'a' && string[i] <= 'z') {
-            string[i] = ((string[i] - 'a' + verschiebung) % 26) + 'a';
+        if (string[i] >= 'a' && string[i] <= 'z')
+        {
+            string[i] = ((string[i] - 'a' - i) % 26) + 'a';
         }
+    }
+}
+
+void ceasarDecryption(char *string, int verschiebung)
+{
+    for (int i = 0; i < 26; i++)
+    {
+        for (int i = 0; string[i] != '\0'; i++)
+        {
+            if (string[i] >= 'a' && string[i] <= 'z')
+            {
+                string[i] = ((string[i] - 'a' + i) % 26) + 'a';
+            }
+        }
+        printf("%s\n", string);
     }
 }
