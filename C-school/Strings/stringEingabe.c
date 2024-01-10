1f1ff4ec98f1bd61903c480ae88153566f864e20
nicolashoeller <hoellr3.0@gmail.com>
2024-01-10 15:40:32 +0100

    modified:   C-school/Strings/stringEingabe.c

--- C-school/Strings/stringEingabe.c
+++ C-school/Strings/stringEingabe.c
@@ -10,16 +10,16 @@ Autor: Nicolas Höller
 
 #define MAX 100
 
-void processString(char*, int);
-void capsAfterSpace(char*);
+void processString(char *, int);
+void capsAfterSpace(char *);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     char string[MAX];
     int option;
 
     printf("Bitte geben Sie einen String ein: ");
-    fgets(string, MAX, stdin);                          // fgets liest auch Leerzeichen ein, stdin ist der Standardinput (Tastatur)
+    fgets(string, MAX, stdin); // fgets liest auch Leerzeichen ein, stdin ist der Standardinput (Tastatur)
 
     printf("Soll der String in Großbuchstaben (1) oder in Kleinbuchstaben (2) ausgegeben werden?\n", string);
     scanf("%d", &option);
@@ -31,7 +31,8 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void capsAfterSpace(char* string){
+void capsAfterSpace(char *string)
+{
     if (string[0] >= 'a' && string[0] <= 'z')
     {
         string[0] = string[0] - 32;
@@ -40,38 +41,35 @@ void capsAfterSpace(char* string){
     {
         if (string[i] == ' ')
         {
-            if (string[i+1] >= 'a' && string[i+1] <= 'z')
+            if (string[i + 1] >= 'a' && string[i + 1] <= 'z')
             {
-                string[i+1] = string[i+1] - 32;
+                string[i + 1] = string[i + 1] - 32;
             }
-            
         }
-        
     }
-    
 }
 
-void processString(char* string, int option)
+void processString(char *string, int option)
 {
     switch (option)
     {
-        case 1:
-            for (int i = 0; string[i] != '\0'; i++)
+    case 1:
+        for (int i = 0; string[i] != '\0'; i++)
+        {
+            if (string[i] >= 'a' && string[i] <= 'z')
             {
-                if (string[i] >= 'a' && string[i] <= 'z')
-                {
-                    string[i] = string[i] - 32;
-                }
+                string[i] = string[i] - 32;
             }
-            break;
-        case 2: 
-            for (int i = 0; string[i] != '\0'; i++)
+        }
+        break;
+    case 2:
+        for (int i = 0; string[i] != '\0'; i++)
+        {
+            if (string[i] >= 'A' && string[i] <= 'Z')
             {
-                if (string[i] >= 'A' && string[i] <= 'Z')
-                {
-                    string[i] = string[i] + 32;
-                }
+                string[i] = string[i] + 32;
             }
-            break;
+        }
+        break;
     }
 }
