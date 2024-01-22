5e2e620b558d24f13811fe4c71f7a54817fb4c88
nicolashoeller <hoellr3.0@gmail.com>
2024-01-22 12:48:01 +0100

    Add function to split lines and count quantity of messages

--- C-school/Files/WhatsApp/whatsappChat.c
+++ C-school/Files/WhatsApp/whatsappChat.c
@@ -7,12 +7,16 @@ Autor: Nicolas Höller
 #include <stdlib.h>
 #include <string.h>
 #define MAX 100
+#define CHATMAX 10000
 
+void splitLines(char*, FILE*, char*, int, int);
 void getUserName(char*);
+void quantityOfMessages(char*, FILE*, char*, int, int);
 
 int main(int argc, char* argv[])
 {
     char userName[MAX];
+    char chatLine[CHATMAX];
     
     FILE* file = fopen(argv[1], "r");
 
@@ -23,12 +27,38 @@ int main(int argc, char* argv[])
     }
 
     getUserName(userName);
+    int temp;
+    int tempQuantity;
+    splitLines(chatLine, file, userName, temp, tempQuantity);
     fclose(file);
     return 0;
 }
 
 void getUserName(char* userName){
     printf("Nach welchem Namen soll gesucht werden?\n");
-    printf("---->");
+    printf("----> ");
     fgets(userName, MAX, stdin);
+}
+
+void splitLines(char* chatLine, FILE* file, char* userName, int temp, int tempQuantity){
+    while ((temp = fgetc(file)) != EOF)
+    {
+        if (fgets(chatLine, strlen(file), file) != NULL)
+        {
+            quantityOfMessages(chatLine, file, userName, temp, tempQuantity);
+        }
+        
+    }
+    printf("%d", tempQuantity);
+    
+}
+
+void quantityOfMessages(char* chatLine, FILE* file, char* userName, int temp, int count){
+    char *posPtr = strstr(userName, chatLine);
+    while (posPtr != NULL)
+    {
+        count++;
+        posPtr = strstr(posPtr + 1, chatLine);
+    }
+    splitLines(chatLine, file, userName, temp, count);
 }
