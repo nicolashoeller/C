af9cb05aed3726295d1d3d3110cfcb35a72b0680
nicolashoeller <hoellr3.0@gmail.com>
2024-02-01 08:38:56 +0100

    Add author and date information to whatsapp1.1.c

--- C-school/Files/WhatsApp/whatsapp1.1.c
+++ C-school/Files/WhatsApp/whatsapp1.1.c
@@ -1,3 +1,9 @@
+/*
+File: whatsapp1.1.c
+Date: 01.02.2024
+Autor: Nicolas Höller
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,49 +19,63 @@ int message_count[MAX_PEOPLE];
 void addOrUpdatePerson(char *);
 void analyzeMessages(FILE *);
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         printf("FALSCHE EINGABE\n");
-        return 1;
+        return -1;
     }
+
     FILE *fp = fopen(argv[1], "r");
-    if (!fp) {
+
+    if (!fp)
+    {
         printf("Fehler beim Öffnen der Datei\n");
-        return 1;
+        return -1;
     }
-    
+
     analyzeMessages(fp);
     fclose(fp);
 
-    for (int i = 0; i < people_count; i++) {
-        printf("%s: %d Nachricht(en)\n", names[i], message_count[i]);
+    for (int i = 0; i < people_count; i++)
+    {
+        printf("%s: %d Nachricht*en\n", names[i], message_count[i]);
     }
     return 0;
 }
 
-void addOrUpdatePerson(char *name) {
+void addOrUpdatePerson(char *name)
+{
     int i;
-    for (i = 0; i < people_count; i++) {
-        if (strcmp(names[i], name) == 0) {
+    for (i = 0; i < people_count; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
             message_count[i]++;
             return;
         }
     }
-    if (people_count < MAX_PEOPLE - 1) {
+    if (people_count < MAX_PEOPLE - 1)
+    {
         strcpy(names[people_count], name);
         message_count[people_count] = 1;
         people_count++;
     }
 }
 
-void analyzeMessages(FILE *fp) {
+void analyzeMessages(FILE *fp)
+{
     char line[MAX_LINE_LENGTH];
-    while (fgets(line, sizeof(line), fp) != NULL) {
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
         char *start = strchr(line, ']');
-        if (start && strlen(start) > 2) {
+        if (start && strlen(start) > 2)
+        {
             start += 2;
             char *end = strchr(start, ':');
-            if (end) {
+            if (end)
+            {
                 int name_length = end - start;
                 char name[MAX_NAME_LENGTH];
                 strncpy(name, start, name_length);
