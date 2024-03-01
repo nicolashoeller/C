81adf7220b7fd5e939c66b5e2b9536614c9eddf6
nicolashoeller <hoellr3.0@gmail.com>
2024-03-01 09:22:29 +0100

    Formatted document

--- C-school/Speicherverwaltung/seitenFehler.c
+++ C-school/Speicherverwaltung/seitenFehler.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fifo(int, int, int*);
-void lru(int, int, int*);
-void optimal(int, int, int*);
-void second_chance(int, int, int*);
+void fifo(int, int, int *);
+void lru(int, int, int *);
+void optimal(int, int, int *);
+void second_chance(int, int, int *);
 int menu();
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int seiten = 0;
     int laenge = 0;
@@ -18,9 +18,10 @@ int main(int argc, char* argv[])
     printf("Länge: ");
     scanf("%d", &laenge);
 
-    int* feld = (int*)malloc(laenge * sizeof(int));
+    int *feld = (int *)malloc(laenge * sizeof(int));
     printf("Referenzfolge eingeben: ");
-    for (int i = 0; i < laenge; i++) {
+    for (int i = 0; i < laenge; i++)
+    {
         scanf("%d", &feld[i]);
     }
 
@@ -47,60 +48,74 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void fifo(int seiten, int laenge, int* feld){
-    int i, j, k, flag=0, count=0;
+void fifo(int seiten, int laenge, int *feld)
+{
+    int i, j, k, flag = 0, count = 0;
     int frame[seiten];
-    for(i=0; i<seiten; i++)
+    for (i = 0; i < seiten; i++)
         frame[i] = -1;
-    
+
     printf("Step\tFrame Contents\n");
-    for(i=0; i<laenge; i++){
-        flag=0;
-        for(j=0; j<seiten; j++){
-            if(frame[j] == feld[i]){
-                flag=1;
+    for (i = 0; i < laenge; i++)
+    {
+        flag = 0;
+        for (j = 0; j < seiten; j++)
+        {
+            if (frame[j] == feld[i])
+            {
+                flag = 1;
                 break;
             }
         }
-        if(flag == 0){
-            frame[count%seiten] = feld[i];
+        if (flag == 0)
+        {
+            frame[count % seiten] = feld[i];
             count++;
-            printf("%d\t", i+1);
-            for(k=0; k<seiten; k++)
-                if(frame[k] == -1)
-                printf("E ");  // Print 'E' for empty slots
-            else
-                printf("%d ", frame[k]);
+            printf("%d\t", i + 1);
+            for (k = 0; k < seiten; k++)
+                if (frame[k] == -1)
+                    printf("E "); // Print 'E' for empty slots
+                else
+                    printf("%d ", frame[k]);
             printf("\n");
         }
     }
     printf("Page Faults in FIFO: %d\n", count);
 }
 
-void lru(int seiten, int laenge, int* feld){
-    int i, j, k, flag=0, count=0, least[seiten];
+void lru(int seiten, int laenge, int *feld)
+{
+    int i, j, k, flag = 0, count = 0, least[seiten];
     int frame[seiten];
-    for(i=0; i<seiten; i++)
+    for (i = 0; i < seiten; i++)
         frame[i] = -1;
-    
+
     printf("Step\tFrame Contents\n");
-    for(i=0; i<laenge; i++){
-        flag=0;
-        for(j=0; j<seiten; j++){
-            if(frame[j] == feld[i]){
-                flag=1;
+    for (i = 0; i < laenge; i++)
+    {
+        flag = 0;
+        for (j = 0; j < seiten; j++)
+        {
+            if (frame[j] == feld[i])
+            {
+                flag = 1;
                 break;
             }
         }
-        if(flag == 0){
-            if(i<seiten){
+        if (flag == 0)
+        {
+            if (i < seiten)
+            {
                 frame[i] = feld[i];
                 least[i] = i;
             }
-            else{
+            else
+            {
                 int min = least[0], pos = 0;
-                for(k=1; k<seiten; k++){
-                    if(least[k]<min){
+                for (k = 1; k < seiten; k++)
+                {
+                    if (least[k] < min)
+                    {
                         min = least[k];
                         pos = k;
                     }
@@ -109,113 +124,135 @@ void lru(int seiten, int laenge, int* feld){
                 least[pos] = i;
             }
             count++;
-            printf("%d\t", i+1);
-            for(k=0; k<seiten; k++)
-                if(frame[k] == -1)
-                printf("E ");  // Print 'E' for empty slots
-            else
-                printf("%d ", frame[k]);
+            printf("%d\t", i + 1);
+            for (k = 0; k < seiten; k++)
+                if (frame[k] == -1)
+                    printf("E "); // Print 'E' for empty slots
+                else
+                    printf("%d ", frame[k]);
             printf("\n");
         }
-        else{
+        else
+        {
             least[j] = i;
         }
     }
     printf("Page Faults in LRU: %d\n", count);
 }
 
-void optimal(int seiten, int laenge, int* feld){
-    int i, j, k, flag=0, count=0, farthest, pos;
+void optimal(int seiten, int laenge, int *feld)
+{
+    int i, j, k, flag = 0, count = 0, farthest, pos;
     int frame[seiten];
-    for(i=0; i<seiten; i++)
+    for (i = 0; i < seiten; i++)
         frame[i] = -1;
-    
+
     printf("Step\tFrame Contents\n");
-    for(i=0; i<laenge; i++){
-        flag=0;
-        for(j=0; j<seiten; j++){
-            if(frame[j] == feld[i]){
-                flag=1;
+    for (i = 0; i < laenge; i++)
+    {
+        flag = 0;
+        for (j = 0; j < seiten; j++)
+        {
+            if (frame[j] == feld[i])
+            {
+                flag = 1;
                 break;
             }
         }
-        if(flag == 0){
-            if(i<seiten){
+        if (flag == 0)
+        {
+            if (i < seiten)
+            {
                 frame[i] = feld[i];
             }
-            else{
-                farthest = i+1;
-                for(j=0; j<seiten; j++){
-                    for(k=i+1; k<laenge; k++){
-                        if(frame[j] == feld[k]){
-                            if(k>farthest){
+            else
+            {
+                farthest = i + 1;
+                for (j = 0; j < seiten; j++)
+                {
+                    for (k = i + 1; k < laenge; k++)
+                    {
+                        if (frame[j] == feld[k])
+                        {
+                            if (k > farthest)
+                            {
                                 farthest = k;
                                 pos = j;
                             }
                             break;
                         }
-                        if(k == laenge-1)
+                        if (k == laenge - 1)
                             pos = j;
                     }
                 }
                 frame[pos] = feld[i];
             }
             count++;
-            printf("%d\t", i+1);
-            for(k=0; k<seiten; k++)
-                if(frame[k] == -1)
-                printf("E ");  // Print 'E' for empty slots
-            else
-                printf("%d ", frame[k]);
+            printf("%d\t", i + 1);
+            for (k = 0; k < seiten; k++)
+                if (frame[k] == -1)
+                    printf("E "); // Print 'E' for empty slots
+                else
+                    printf("%d ", frame[k]);
             printf("\n");
         }
     }
     printf("Page Faults in Optimal: %d\n", count);
 }
 
-void second_chance(int seiten, int laenge, int* feld){
-    int i, j, flag1=0, flag2=0, count=0, pos=0;
+void second_chance(int seiten, int laenge, int *feld)
+{
+    int i, j, flag1 = 0, flag2 = 0, count = 0, pos = 0;
     int frame[seiten], second_chance[seiten];
-    for(i=0; i<seiten; i++){
+    for (i = 0; i < seiten; i++)
+    {
         frame[i] = -1;
         second_chance[i] = 0;
     }
-    
+
     printf("Step\tFrame Contents\n");
-    for(i=0; i<laenge; i++){
-        flag1=0, flag2=0;
-        for(j=0; j<seiten; j++){
-            if(frame[j] == feld[i]){
-                flag1=1, flag2=1;
+    for (i = 0; i < laenge; i++)
+    {
+        flag1 = 0, flag2 = 0;
+        for (j = 0; j < seiten; j++)
+        {
+            if (frame[j] == feld[i])
+            {
+                flag1 = 1, flag2 = 1;
                 second_chance[j] = 1;
                 break;
             }
         }
-        if(flag1 == 0){
-            for(j=0; j<seiten; j++){
-                if(frame[j] == -1){
+        if (flag1 == 0)
+        {
+            for (j = 0; j < seiten; j++)
+            {
+                if (frame[j] == -1)
+                {
                     count++;
                     frame[j] = feld[i];
                     second_chance[j] = 1;
-                    flag2=1;
+                    flag2 = 1;
                     break;
                 }
             }
         }
-        if(flag2 == 0){
-            while(second_chance[pos] == 1){
+        if (flag2 == 0)
+        {
+            while (second_chance[pos] == 1)
+            {
                 second_chance[pos] = 0;
-                pos = (pos+1)%seiten;
+                pos = (pos + 1) % seiten;
             }
             frame[pos] = feld[i];
             second_chance[pos] = 1;
-            pos = (pos+1)%seiten;
+            pos = (pos + 1) % seiten;
             count++;
         }
-        printf("%d\t", i+1);
-        for(j=0; j<seiten; j++)
-            if(frame[j] == -1)
-                printf("E ");  // Print 'E' for empty slots
+        printf("%d\t", i + 1);
+        for (j = 0; j < seiten; j++)
+            if (frame[j] == -1)
+                printf("E "); // Print 'E' for empty slots
             else
                 printf("%d ", frame[j]);
         printf("\n");
@@ -223,7 +260,8 @@ void second_chance(int seiten, int laenge, int* feld){
     printf("Page Faults in Second Chance: %d\n", count);
 }
 
-int menu(){
+int menu()
+{
     int auswahl = 0;
     printf("1. FIFO\n");
     printf("2. LRU\n");
