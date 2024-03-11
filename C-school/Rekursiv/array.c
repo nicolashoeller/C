9a568ad90a1c01e481669b297547c19e774f3b44
nicolashoeller <hoellr3.0@gmail.com>
2024-03-11 12:51:32 +0100

    Add arraySum function to calculate the sum of an array recursively

---
+++ C-school/Rekursiv/array.c
@@ -0,0 +1,23 @@
+/*
+File: array.c
+Date: 11.03.2024
+Autor: Nicolas Höller
+*/
+
+#include <stdio.h>
+
+int arraySum(int array[], int n);
+
+int main() {
+    int array[] = {1, 2, 3, 4, 5};
+    int n = sizeof(array) / sizeof(array[0]);
+    printf("Sum of array is %d", arraySum(array, n));
+    return 0;
+}
+
+int arraySum(int array[], int n) {
+    if (n <= 0)
+        return 0;
+    else
+        return (arraySum(array, n - 1) + array[n - 1]);
+}
