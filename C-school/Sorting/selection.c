febe2b6e8d404b46e6f94ec77a447a79f57df863
nicolashoeller <hoellr3.0@gmail.com>
2024-03-13 16:02:12 +0100

    Add selection sort algorithm implementation

---
+++ C-school/Sorting/selection.c
@@ -0,0 +1,53 @@
+/*
+File: selection.c
+Date: 13.03.2024
+Autor: Nicolas Höller
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE 10
+
+void selection_sort(int arr[], int n)
+{
+    int min_idx, temp;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main()
+{
+    int arr[ARRAY_SIZE];
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 100;
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    selection_sort(arr, n);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
