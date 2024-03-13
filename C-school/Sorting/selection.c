3b5e835c1759abf1012dd2362bcfc448a706e057
nicolashoeller <hoellr3.0@gmail.com>
2024-03-13 16:05:16 +0100

    Refactor selection_sort function

--- C-school/Sorting/selection.c
+++ C-school/Sorting/selection.c
@@ -10,23 +10,7 @@ Autor: Nicolas Höller
 
 #define ARRAY_SIZE 10
 
-void selection_sort(int arr[], int n)
-{
-    int min_idx, temp;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        min_idx = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-        }
-        temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-    }
-}
+void selection_sort(int *, int);
 
 int main()
 {
@@ -50,4 +34,24 @@ int main()
         printf("%d ", arr[i]);
 
     return 0;
+}
+
+void selection_sort(int *arr, int n)
+{
+    int min_idx, temp;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
 }
