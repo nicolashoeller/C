6a405dbe0cf990ea73846a3422604e6a4a6cf0a9
nicolashoeller <hoellr3.0@gmail.com>
2024-03-20 15:09:30 +0100

    Refactor insertion sort algorithm

--- C-school/Sorting/insertion.c
+++ C-school/Sorting/insertion.c
@@ -4,32 +4,64 @@ Date: 18.03.2024
 Autor: Nicolas Höller
 */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 
-#define ARR_LENGHT 20
+#define ARR_LENGTH 20
 
-void insertion();
-void verschieben(int*, int, int);
-void iniArray(int*);
+void insertion(int* array);
+void verschieben(int* array, int index1, int index2);
+void iniArray(int* array);
+void printArray(int* array);
 
 int main(int argc, char* argv[])
 {
-    int array[ARR_LENGHT];
+    int array[ARR_LENGTH];
 
     iniArray(array);
+
+    printf("Before sorting: \n");
+
+    printArray(array);
+
+    insertion(array);
+
+    printf("After sorting: \n");
+    
+    printArray(array);
+
+    return 0;
+}
+
+void insertion(int* array) {
+    int i, j;
+    for (i = 1; i < ARR_LENGTH; i++) {
+        j = i;
+        while (j > 0 && array[j - 1] > array[j]) {
+            verschieben(array, j, j - 1);
+            j--;
+        }
+    }
 }
 
 void verschieben(int* array, int index1, int index2){
-    
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
 }
 
 void iniArray(int * array){
-    for (int i = 0; i < ARR_LENGHT; i++)
+    for (int i = 0; i < ARR_LENGTH; i++)
     {
         array[i] = rand() % 100;
     }
+}
 
-    return;
+void printArray(int* array){
+    for (int i = 0; i < ARR_LENGTH; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    
+    printf("\n");
 }
