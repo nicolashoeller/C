2e80d344b1ec91d555face492b29ea85821e73dd
nicolashoeller <hoellr3.0@gmail.com>
2024-04-27 17:09:58 +0200

    Add client address output

--- C-personal/HTTP-Server/Server_1/server.c
+++ C-personal/HTTP-Server/Server_1/server.c
@@ -12,54 +12,56 @@ int main(int argc, char const *argv[])
 {
     int listenfd, connfd, n;
     struct sockaddr_in servaddr;
-    uint8_t buff[MAXLINE+1];
-    uint8_t recvline[MAXLINE+1];
+    uint8_t buff[MAXLINE + 1];
+    uint8_t recvline[MAXLINE + 1];
 
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         error_and_die("socket error.");
-    
+
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERVER_PORT);
 
-    if ((bind(listenfd, (SA*) &servaddr, sizeof(servaddr))) < 0)
+    if ((bind(listenfd, (SA *)&servaddr, sizeof(servaddr))) < 0)
         error_and_die("bind error.");
-    
+
     if ((listen(listenfd, 10)) < 0)
         error_and_die("listen error.");
 
-    for ( ; ; )
+    for (;;)
     {
         struct sockaddr_in addr;
-        socklen_t addr_len;
+        socklen_t addr_len = sizeof(addr); // Initialize addr_len
+        char client_address[MAXLINE + 1];
 
-        printf("-----------------\nWaiting for connection on port %d\n-----------------\n", SERVER_PORT);
+        printf("----------------------------------\nWaiting for connection on port %d\n\n", SERVER_PORT);
         fflush(stdout);
-        connfd = accept(listenfd, (SA *) NULL, NULL);
+        connfd = accept(listenfd, (SA *)&addr, &addr_len);
+
+        inet_ntop(AF_INET, &addr.sin_addr, client_address, MAXLINE); // Use addr.sin_addr instead of addr
+        printf("Client address: %s\n", client_address);
 
         memset(recvline, 0, MAXLINE);
 
-        while ((n = read(connfd, recvline, MAXLINE-1)) > 0)
+        while ((n = read(connfd, recvline, MAXLINE - 1)) > 0)
         {
             fprintf(stdout, "\n%s\n\n%s", bin2hex(recvline, n), recvline);
 
-            if (recvline[n-1] == '\n')
+            if (recvline[n - 1] == '\n')
                 break;
-            
+
             memset(recvline, 0, MAXLINE);
         }
 
         if (n < 0)
             error_and_die("read error.");
-        
-        //response
 
-        snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nResponse");
+        // response
 
-        write(connfd, (char*)buff, strlen((char*)buff));
+        snprintf((char *)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nResponse");
+
+        write(connfd, (char *)buff, strlen((char *)buff));
         close(connfd);
     }
-    
-    
 }
