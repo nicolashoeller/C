0ac9e1b6143baf97e67dd1091229e38c52821248
nicolashoeller <hoellr3.0@gmail.com>
2024-04-27 16:39:15 +0200

    Add common.h file for common functions in HTTP server

---
+++ C-personal/HTTP-Server/Server_1/common.h
@@ -0,0 +1,26 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+#define MAXLINE 4096
+#define SA struct sockaddr
+
+void error_and_die(const char *fmt, ...);
+char *bin2hex(const unsigned char *input, size_t len);
+
+#endif
