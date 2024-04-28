5a9648a1aebda5f7ba9124f29c23ca061da0cf5d
nicolashoeller <hoellr3.0@gmail.com>
2024-04-28 13:54:38 +0200

    Refactor client.c to include chat functionality and improve user experience

--- C-personal/HTTP-Server/Chat/client.c
+++ C-personal/HTTP-Server/Chat/client.c
@@ -11,10 +11,12 @@ Autor: Nicolas Höller
 int main(int argc, char *argv[])
 {
     int socketfd, result;
-    char*message;
-    char buffer[4096];
     char*ip_address = argv[1];
+    char*line = NULL;
+    char buffer[4096];
     struct sockaddr_in *servaddr;
+    size_t lineSize = 0;
+    ssize_t charCount;
 
     if (argc != 2)
         error_and_die("[!] Usage: %s <server address>\n", argv[0]);
@@ -32,13 +34,21 @@ int main(int argc, char *argv[])
     
     printf("[*] Successfully established connection with server at %s:%d\n", ip_address, SERVER_PORT);
 
+    printf("[*] Welcome in the chat room... (type exit to disconnect)\n\n");
 
-    message = "GET \\ HTTP/1.1\r\nHost:google.com\r\n\r\n";
-    send(socketfd, message, strlen(message), 0);
+    while (true)
+    {
+        if ((charCount = getline(&line, &lineSize, stdin)) > 0){
+            if (!strcmp(line, "exit\n")){
+                printf("[*] Disconnecting...\n");
+                break;
+            }
+            
+            size_t wasSend = send(socketfd, line, charCount, 0);
+        }
+    }
 
-    recv(socketfd, buffer, sizeof(buffer), 0);
-
-    printf("\nResponse: %s", buffer);
+    close(socketfd);
 
     return 0;
 }
